c69098286f301ed40c305c8cf94938b6deb11944
Truong Tran <truongtran.cs@gmail.com>
2016-04-05 17:28:08 +0700

    add loginController

---
+++ src/main/java/com/kms/launch/web/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.kms.launch.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
+
+/**
+ * Created by truongtran on 4/5/2016.
+ */
+@Controller
+public class LoginController {
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView getLoginPage(@RequestParam Optional<String> error) {
+        return new ModelAndView("login", "error", error);
+    }
+}
